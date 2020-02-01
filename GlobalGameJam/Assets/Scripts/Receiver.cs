6cfadf4ab7a155f8d72bc416a8149476bd50dfa1
Dominique <dominique.dickner>
2020-02-01 16:04:33 -0500

    Receiver update

--- GlobalGameJam/Assets/Scripts/Receiver.cs
+++ GlobalGameJam/Assets/Scripts/Receiver.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Receiver : MonoBehaviour
 {
+    public bool isBlocked;
+    public bool wantsItem;
     public void ReceiveItem()
     {
         print("Item Received");
