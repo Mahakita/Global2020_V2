e9eda4b6a8cfbd79d5c62d7d1fe8e3e9de5a5de4
calvin <calvinator974@gmail.com>
2020-02-01 22:48:30 -0500

    Dispensers update, Items,Bots

---
+++ GlobalGameJam/Assets/Scripts/Dispencers/BookDispenser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookDispenser : MonoBehaviour
+{
+    public Receiver receiver;
+    public GameObject Book;
+    public BoxCollider BookBoxCollider;
+    public float creationProgress;
+    public float speed = 10f;
+    void Update()
+    {
+        if (receiver.wantsItem == false)
+        {
+            creationProgress += speed * Time.deltaTime;
+            if (creationProgress >= 100f)
+            {
+                Book.GetComponent<BoxCollider>().enabled = true;
+                Instantiate(Book, transform.position + new Vector3(0, 2, 0), Quaternion.identity);
+                creationProgress = 0f;
+            }
+
+        }
+
+
+    }
+
+}
