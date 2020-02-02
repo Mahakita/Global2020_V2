1db5edecc182dd624fa0085926ee8cf94772177f
Dominique <dominique.dickner>
2020-02-01 22:51:00 -0500

    Dom Update 4.0

--- GlobalGameJam/Assets/Scripts/PickUpper.cs
+++ GlobalGameJam/Assets/Scripts/PickUpper.cs
@@ -10,110 +10,87 @@ public class PickUpper : MonoBehaviour
 
     private bool holdsItem = false;
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (enter)
-            print(other.name + " Has Entered");
-    }
-    void OnTriggerStay(Collider other)
-    {
-        if (holdsItem != true)
-        {
-            if (other.GetComponent<Item>() && Input.GetKey(KeyCode.E))
-            {
-                print(other.name + " Get Item");
-                Destroy(other.gameObject);
-                holdsItem = true;
-                return;
-            }
-            return;
-        }
-        if (holdsItem == true)
-        {
-            if (other.GetComponent<Receiver>() && Input.GetKey(KeyCode.E))
-            {
-                print(gameObject.name + " Drop Item");
-                // if receive Item returns true
-                    // remove Item
-
-                // if receive item = false
-                    // keep item
-                other.GetComponent<Receiver>().ReceiveItem();
-                holdsItem = false;
-                return;
-            }
-            return;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (exit)
-            print(other.name + " Has Exited");
-    }
-
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Collider[] colliders;
+            colliders = Physics.OverlapSphere(transform.position, 0.25f);
+            print(colliders.Length);
+
+            if (holdsItem)
+            {
+                foreach (Collider c in colliders)
+                {
+                    if (c.GetComponent<Receiver>())
+                    {
+                        // CASE 1 : Receiver wants Item -> Give Item to receiver
+                        if (c.GetComponent<Receiver>().ReceiveItem())
+                        {
+                            Destroy(transform.GetChild(0).gameObject);
+                            holdsItem = false;
+                        }
+                        // CASE 2 : Receiver does not want Item -> Do Nothing
+                        else
+                        {
+                            // do nothing
+                        }
+
+                        return;
+                    }
+                }
+                // CASE 3 : No receiver found, Drop Item
+                {
+                    //holditem to false
+                    Transform heldItem = transform.GetChild(0);
+                    heldItem.transform.localPosition = new Vector3(-1, 0, 0);
+
+                    // un-parent the item
+                    heldItem.transform.parent = null;
+
+                    holdsItem = false;
+
+                    // reenable colision component on item
+                    heldItem.GetComponent<BoxCollider>().enabled = true;
+                    print(gameObject.name + " Drop Item");
+                }
+
+            }
+            else
+            {
+                // no Item, try to pickup
+                foreach (Collider c in colliders)
+                {
+                    if (c.GetComponent<Item>())
+                    {
+                        print(c.name + " Get Item");
+                        holdsItem = true;
+                        c.transform.parent = gameObject.transform;
+                        c.transform.localPosition = new Vector3(0, 0.75f, 0);
+                        c.GetComponent<BoxCollider>().enabled = false;
+                        return;
+                    }
+                }
+            }
+
+            print("No action taken");
+
+        }
        
+        /*
+        
+        Si on appuie sur e
+            1) on check si on a un objet
+                On check si on trigger un bot
+                    si oui, on lui donne le shit
+
+                    Si non, on drop
+
+            on check si on est dans le trigger dun objet
+                si oui, on le grab
+
+
+
+         * */
     }
-  
-
-    //void Update()
-    //{
-    //    if (Input.GetKey("E") && isOverlap != false);
-    //    {
-    //        hasItem;
-    //    }
-    //    else
-    //    {
-    //        return;
-    //    }
-    //}
-
-    //void hasItem()
-    //{
-    //    other.GetComponent<Item>;
-    //}
-
-    //void Update()
-    //{
-    //    if (currentOverlappingItem == null)
-    //    {
-    //        print("null");
-    //    }
-    //    else
-    //    {
-    //        print(currentOverlappingItem.name);
-    //    }
-
-    //    //Debug.Log("Is Overlapping Item = " + currentOverlappingItem == null);
-    //}
-
-
-    //void OnTriggerEnter(Collider col)
-    //{
-    //    if (currentOverlappingItem != null)
-    //    {
-    //        return;
-    //    }
-
-    //    currentOverlappingItem = col.GetComponent<Item>();
-    //    if (currentOverlappingItem != null)
-    //    {
-    //        Debug.Log("On Trigger Item");
-    //    }
-    //}
-
-    //void OnTriggerExit(Collider col)
-    //{
-    //    if (currentOverlappingItem == null)
-    //    {
-    //        return;
-    //    }
-
-    //    if (col == currentOverlappingItem.GetComponent<Collider>())
-    //    {
-    //        currentOverlappingItem = null;
-    //    }
-    //}
 }
--- GlobalGameJam/Assets/Scripts/Receiver.cs
+++ GlobalGameJam/Assets/Scripts/Receiver.cs
@@ -10,68 +10,66 @@ public class Receiver : MonoBehaviour
     private IEnumerator coroutine;
     public float TempsMin = 15f;
     public float TempsMax = 25f;
-    Renderer renderer;
+    Renderer myRenderer;
     public Material happyMat;
     public Material wantMat;
 
 
     public void Start()
     {
-        renderer = GetComponent<Renderer>();
+        myRenderer = GetComponent<Renderer>();
 
         // start coroutine for wants item
-        // - After 0 seconds, prints "Starting 0.0 seconds"
-        // - After 0 seconds, prints "Coroutine started"
-        // - After 2 seconds, prints "Coroutine ended: 8.0 seconds"
-        print("Starting " + Time.time + " seconds");
+        //print("Starting " + Time.time + " seconds");
 
         // Start function WaitAndPrint as a coroutine.
 
         coroutine = WaitAndPrint(Random.Range(TempsMin, TempsMax));
         StartCoroutine(coroutine);
 
-        print("Coroutine started");
+        //print("Coroutine started");
     }
 
     private IEnumerator WaitAndPrint(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        print("Coroutine ended: " + Time.time + " seconds");
+        //print("Coroutine ended: " + Time.time + " seconds");
         // Wants Item:
         if (wantsItem != true)
         {
             wantsItem = true;
-            renderer.material = wantMat;
+            myRenderer.material = wantMat;
         }
     }
 
    
     //receive Item will return a bool
-    public void ReceiveItem()
+    public bool ReceiveItem()
     {
         // if wants item 
-        if(wantsItem != false)
+        if(wantsItem == true)
         {
             // set want item = false
             wantsItem = false;
             // set material to happy
-            renderer.material = happyMat;
+            myRenderer.material = happyMat;
 
             // start coroutine for wants item
-            print("Starting " + Time.time + " seconds");
+            //print("Starting " + Time.time + " seconds");
             coroutine = WaitAndPrint(Random.Range(TempsMin, TempsMax));
             StartCoroutine(coroutine);
+            //print("Coroutine started");
+            //print("Item Received");
 
-            print("Coroutine started");
-            print("Item Received");
             // return true
-            return;
+            return true;
         }
         // if not wants item 
         else
         {
+            //print("I don't want your item");
             //return false
-            return;
+            return false;
         }
     }
 }
