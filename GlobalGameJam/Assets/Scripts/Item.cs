0f03e5c738948928821aff3b2444c8fae5fb1a29
Dominique <dominique.dickner>
2020-02-02 15:51:19 -0500

    item script update

--- GlobalGameJam/Assets/Scripts/Item.cs
+++ GlobalGameJam/Assets/Scripts/Item.cs
@@ -6,7 +6,7 @@ public class Item : MonoBehaviour
 {
     public ItemType ItemID;
 
-    // 0 = floppy, 1 = book, 2 = folder, 3 = cookie
+    // 0 = none, 1 = folder, 2 = book, 3 = floppy, 4 = cokkie, 5 = any
 }
 
 public enum ItemType { None, Folder, Book, FloppyDisk, Cookie, Any}
